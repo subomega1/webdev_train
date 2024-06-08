e07666acf847f5d24382c9ed903c2a74653ecd6b
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-06-08 08:26:49 +0100

    done 3ed lesson

--- js train/tempconver/index.js
+++ js train/tempconver/index.js
@@ -1,1 +1,24 @@
-function convert(){}
+const text_box= document.getElementById("textbox");
+const Fahrenheit=document.getElementById("toFahrenheit");
+const Celsius=document.getElementById("toCelsius");
+const resualt =document.getElementById("resualt");
+
+let temp;
+
+function convert(){
+  if(Fahrenheit.checked)
+    {
+      temp = Number(text_box.value);
+      temp =  temp * 9/ 5 + 32
+      resualt.textContent = temp + "°F"
+    }
+    else if(Celsius.checked){
+      temp = Number(text_box.value);
+      temp =  (temp -32) *(5/9)
+      resualt.textContent = temp + "°C"
+      
+    }
+    else{
+      resualt.textContent = "Select a unit"
+    }
+}
